81f0d7edd224ed4e1ccfd231cc59f50d07cacce2
Srivishnu Ayyagari <70939201+srivishnua-lambdatest@users.noreply.github.com>
2022-01-24 20:37:37 +0530

    Update index.js
    Changes in the code alignment for better readability.

--- index.js
+++ index.js
@@ -21,52 +21,49 @@ const webdriver = require('selenium-webdriver');
 */
 
 // username: Username can be found at automation dashboard
-const USERNAME = '{username}';
+const USERNAME = 'srivishnua';
 
 // AccessKey:  AccessKey can be generated from automation dashboard or profile section
-const KEY = '{accessKey}';
+const KEY = 'lRPrFiIHat1GfMOOMISoBEcyPoa9XsABtLjAGw4flFgW2PjG1P';
 
 // gridUrl: gridUrl can be found at automation dashboard
 const GRID_HOST = 'hub.lambdatest.com/wd/hub';
 
+const gridUrl = 'https://' + USERNAME + ':' + KEY + '@' + GRID_HOST;
+
+const capabilities = {
+    platform: 'windows 10',
+    browserName: 'chrome',
+    version: '67.0',
+    resolution: '1280x800',
+    network: true,
+    visual: true,
+    console: true,
+    video: true,
+    name: 'Test 1', // name of the test
+    build: 'NodeJS build' // name of the build
+}
+
+// setup and build selenium driver object 
+const driver = new webdriver.Builder()
+    .usingServer(gridUrl)
+    .withCapabilities(capabilities)
+    .build();
+
 function searchTextOnGoogle() {
-
-    // Setup Input capabilities
-    const capabilities = {
-        platform: 'windows 10',
-        browserName: 'chrome',
-        version: '67.0',
-        resolution: '1280x800',
-        network: true,
-        visual: true,
-        console: true,
-        video: true,
-        name: 'Test 1', // name of the test
-        build: 'NodeJS build' // name of the build
-    }
-
-    // URL: https://{username}:{accessToken}@beta-hub.lambdatest.com/wd/hub
-    const gridUrl = 'https://' + USERNAME + ':' + KEY + '@' + GRID_HOST;
-
-    // setup and build selenium driver object 
-    const driver = new webdriver.Builder()
-        .usingServer(gridUrl)
-        .withCapabilities(capabilities)
-        .build();
-
     // navigate to a url, search for a text and get title of page
-    driver.get('https://www.google.com/ncr').then(function() {
-        driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest\n').then(function() {
-            driver.getTitle().then(function(title) {
-                setTimeout(function() {
+    driver.get('https://www.google.com/ncr').then(function () {
+        driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest\n').then(function () {
+            driver.getTitle().then(function (title) {
+                setTimeout(function () {
                     console.log(title);
                     driver.executeScript('lambda-status=passed');
                     driver.quit();
                 }, 5000);
             });
         });
-    }).catch(function(err){
-        console.log("test failed with reason "+err)
+    }).catch(function (err) {
+        console.log("test failed with reason " + err)
         driver.executeScript('lambda-status=failed');
         driver.quit();
     });
