0485ac4f1224ce6883ab7345963928a2513d131e
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2023-04-21 10:06:44 +0000

    Updated Dependencies

--- index.js
+++ index.js
@@ -9,16 +9,8 @@
     -------
     Execute NodeJS Automation Tests on LambdaTest Distributed Selenium Grid 
 */
-const webdriver = require('selenium-webdriver');
 
-/*
-    Setup remote driver
-    Params
-    ----------
-    platform : Supported platform - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
-    browserName : Supported platform - (chrome, firefox, Internet Explorer, MicrosoftEdge, Safari)
-    version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
-*/
+const webdriver = require('selenium-webdriver');
 
 // username: Username can be found at automation dashboard
 const USERNAME = process.env.LT_USERNAME;
@@ -29,53 +21,48 @@ const KEY = process.env.LT_ACCESS_KEY;
 // gridUrl: gridUrl can be found at automation dashboard
 const GRID_HOST = 'hub.lambdatest.com/wd/hub';
 
-
-function searchTextOnGoogle() {
-
+async function searchTextOnGoogle() {
     // Setup Input capabilities
+
     const capabilities = {
-        platform: 'windows 10',
-        browserName: 'chrome',
-        version: 'latest',
-        resolution: '1280x800',
-        // geoLocation : "US",
-        // network: true,
-        // visual: true,
-        // console: true,
-        // video: true,
-        name: 'Test 1', // name of the test
-        build: 'NodeJS build' // name of the build
+        "browserName": "Chrome",
+        "browserVersion": "latest",
+        "LT:Options": {
+            // "platformName": "Windows 10",
+            name: 'Test 1', // name of the test
+            build: 'NodeJS build', // name of the build
+            "project": "Untitled",
+            "w3c": true,
+            "plugin": "node_js-node_js"
+        }
     }
 
     // URL: https://{username}:{accessToken}@beta-hub.lambdatest.com/wd/hub
     const gridUrl = 'https://' + USERNAME + ':' + KEY + '@' + GRID_HOST;
 
-    // setup and build selenium driver object 
+    // Setup and build selenium driver object
     const driver = new webdriver.Builder()
         .usingServer(gridUrl)
         .withCapabilities(capabilities)
         .build();
 
-    // navigate to a url, click on the first and second list items and add a new one in the list.
-    driver.get('https://lambdatest.github.io/sample-todo-app/').then(function() {
-        driver.findElement(webdriver.By.name('li1')).click().then(function(){
-           console.log("Successfully clicked first list item.");
-        });
-        driver.findElement(webdriver.By.name('li2')).click().then(function(){
-            console.log("Successfully clicked second list item.");
-         });
+    try {
+        // Navigate to a URL, click on the first and second list items and add a new one in the list.
+        await driver.get('https://lambdatest.github.io/sample-todo-app/');
+        await driver.findElement(webdriver.By.name('li1')).click();
+        console.log("Successfully clicked first list item.");
+        await driver.findElement(webdriver.By.name('li2')).click();
+        console.log("Successfully clicked second list item.");
 
-         driver.findElement(webdriver.By.id('sampletodotext')).sendKeys('Complete Lambdatest Tutorial\n').then(function(){
-             driver.findElement(webdriver.By.id('addbutton')).click().then(function(){
-                 console.log("Successfully added a new task.");
-             })
-         });
-
-    }).catch(function(err){
-        console.log("test failed with reason "+err)
-        driver.executeScript('lambda-status=failed');
-        driver.quit();
-    });
+        await driver.findElement(webdriver.By.id('sampletodotext')).sendKeys('Complete Lambdatest Tutorial\n');
+        await driver.findElement(webdriver.By.id('addbutton')).click();
+        console.log("Successfully added a new task.");
+    } catch (err) {
+        console.log("test failed with reason " + err);
+        await driver.executeScript('lambda-status=failed');
+    } finally {
+        await driver.quit();
+    }
 }
-searchTextOnGoogle();
 
+searchTextOnGoogle();
